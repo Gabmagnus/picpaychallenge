69874bec5e42d65dc1f4cccc0c539e1b3fb12170
Gabmagnus <gabmagnusoliveira@gmail.com>
2024-07-23 23:26:07 +0100

    create UserRepository

---
+++ src/main/java/com/gabma/picpay_challenge/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.gabma.picpay_challenge.repositories;
+
+import com.gabma.picpay_challenge.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(Long id);
+}
---
+++ src/test/java/com/gabma/picpay_challenge/PicpayChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabma.picpay_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
